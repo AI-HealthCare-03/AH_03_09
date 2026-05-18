07243e55a5bc3162f8a5161c327e359651b71401
cherryyoun33 <cherryyoun33@gmail.com>
2026-05-18 13:52:28 +0900

    fix: resolve merge conflict in v1 router

--- app/apis/v1/__init__.py
+++ app/apis/v1/__init__.py
@@ -1,18 +1,13 @@
 from fastapi import APIRouter
 
 from app.apis.v1.auth_routers import auth_router
-<<<<<<< Updated upstream
 from app.apis.v1.chat_routers import chat_router
-=======
 from app.apis.v1.guide_routers import guide_router
->>>>>>> Stashed changes
 from app.apis.v1.user_routers import user_router
 
 v1_routers = APIRouter(prefix="/api/v1")
+
 v1_routers.include_router(auth_router)
 v1_routers.include_router(user_router)
-<<<<<<< Updated upstream
 v1_routers.include_router(chat_router)
-=======
 v1_routers.include_router(guide_router)
->>>>>>> Stashed changes
