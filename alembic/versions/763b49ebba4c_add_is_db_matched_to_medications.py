db1731882888f9de37994327903d128574cc7837
kmh8405 <kmh.ssem8405@gmail.com>
2026-06-04 18:04:25 +0900

    ✨ feat(ocr): medications 테이블에 is_db_matched 컬럼 추가
    DB 매칭 성공 여부를 저장해 FE에서 원문 유지 약물을 식별할 수 있도록 함
    NULL = 레거시 데이터, TRUE = DB 매칭 성공, FALSE = 원문 유지

---
+++ alembic/versions/763b49ebba4c_add_is_db_matched_to_medications.py
@@ -0,0 +1,26 @@
+"""add_is_db_matched_to_medications
+
+Revision ID: 763b49ebba4c
+Revises: 6eeb06bb552d
+Create Date: 2026-06-04 09:01:59.242901
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '763b49ebba4c'
+down_revision: Union[str, Sequence[str], None] = '6eeb06bb552d'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column('medications', sa.Column('is_db_matched', sa.Boolean(), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column('medications', 'is_db_matched')
