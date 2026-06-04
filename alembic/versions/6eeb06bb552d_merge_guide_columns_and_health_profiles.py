96002690b9f651b3cf6effdece2d23ad8fd7ce9c
kmh8405 <kmh.ssem8405@gmail.com>
2026-06-04 15:46:15 +0900

    🎨 style: alembic migration 파일 ruff 포맷 적용

--- alembic/versions/6eeb06bb552d_merge_guide_columns_and_health_profiles.py
+++ alembic/versions/6eeb06bb552d_merge_guide_columns_and_health_profiles.py
@@ -5,6 +5,7 @@ Revises: e1f2a3b4c5d6, 588eaa5d1567
 Create Date: 2026-06-04 06:25:27.189090
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '6eeb06bb552d'
-down_revision: Union[str, Sequence[str], None] = ('e1f2a3b4c5d6', '588eaa5d1567')
+revision: str = "6eeb06bb552d"
+down_revision: Union[str, Sequence[str], None] = ("e1f2a3b4c5d6", "588eaa5d1567")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
