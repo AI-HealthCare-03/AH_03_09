91675432dc4f36f873506f04b0dc80a2e12e81cd
kmh8405 <kmh.ssem8405@gmail.com>
2026-06-11 17:55:27 +0900

    🐛 fix(ocr): guide_job_id flush 누락으로 DB 미저장 문제 수정

--- app/services/ocr/document_service.py
+++ app/services/ocr/document_service.py
@@ -334,6 +334,7 @@ class OcrDocumentService:
             guide_resp = await GuideService().create_guide_job(guide_req)
             guide_job_id = guide_resp.job_id
             doc.guide_job_id = guide_job_id
+            await self.session.flush()
 
         if trigger_chatbot_context:
             logger.info(
